a872bd31842b62f0aa6e8f0672998efe38b24522
Sven Tiigi <sven.tiigi@gmail.com>
2018-01-12 11:39:10 +0100

    Added escapeSlackCharacters function

--- Sources/PerfectSlackAPIClient/Extensions/StringExtension.swift
+++ Sources/PerfectSlackAPIClient/Extensions/StringExtension.swift
@@ -9,14 +9,24 @@ import Foundation
 
 public extension String {
     
+    /// String MarkdownFormat Enumeration
     public enum MarkdownFormat {
+        /// ```pre```
         case pre
+        /// `code`
         case code
+        /// _italic_
         case italic
+        /// *bold*
         case bold
+        /// ~strike~
         case strike
     }
     
+    /// Convert the String to a specific Markdown format
+    ///
+    /// - Parameter format: The Markdown format
+    /// - Returns: The updated string in markdown format
     func toMarkdown(format: MarkdownFormat) -> String {
         switch format {
         case .pre:
@@ -30,6 +40,14 @@ public extension String {
         case .strike:
             return "~\(self)~"
         }
+    }
+    
+    /// Escape characters to match the guidelines for a Slack Message
+    /// Slack: There are three characters you must convert to HTML entities and only three: &, <, and >.
+    mutating func escapeSlackCharacters() {
+        self = self.replacingOccurrences(of: "&", with: "&amp;")
+            .replacingOccurrences(of: "<", with: "&lt;")
+            .replacingOccurrences(of: ">", with: "&gt;")
     }
     
 }
