a8cd606404650983251ff8db7a167a80e287f750
Cap.雪ノ下八幡 <ywq44@iCloud.com>
2019-06-14 10:32:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/SvenTiigi/PerfectAPIClient.git", from: "1.0.0")
+        .package(url: "https://github.com/CaptainYukinoshitaHachiman/PerfectAPIClient.git", from: "1.0.0")
     ],
     targets: [
         .target(
